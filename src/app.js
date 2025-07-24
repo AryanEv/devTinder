125f1cb701da6148d50fc627fd431b351742452c
AryanEv <aryan.ev@supernovagenset.com>
2025-07-24 11:48:59 +0530

    created an express server

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app=express();
+
+app.get("/",(req,res)=>{
+    res.send("hello world !");
+});
+
+app.use("/about",(req,res)=>{
+    res.send("about page");
+});
+
+app.use("/contact",(req,res)=>{
+    res.send("contact page");
+});
+
+app.listen(3000,()=>{
+    console.log("server is running on port 3000");
+});
