9dfc467b4a72f1adf312026dd0be4e6ac9f5456c
AryanEv <aryan.ev@supernovagenset.com>
2025-07-24 14:20:52 +0530

    create a server

--- src/app.js
+++ src/app.js
@@ -9,10 +9,14 @@ app.use("/about",(req,res)=>{
     res.send("about page");
 });
 
-app.use("/contact",(req,res)=>{
+app.use("/contact",(req,res  )=>{
     res.send("contact page");
 });
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000");
 });
+
+// order of rounting to write is matter a lot
+
+
